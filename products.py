cf5edbe3f8628e68e1aecc13ba206136c4f3fbbd
Yanny <SUM_1026@yahoo.com.hk>
2024-04-05 18:17:51 +0800

    add for loop

--- products.py
+++ products.py
@@ -1,4 +1,5 @@
 products = []
+count = 0
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
@@ -6,3 +7,10 @@ while True:
 	price = input('請輸入價錢:')
 	products.append([name, price])
 print(products)
+
+for p in products:
+	print(p[0], '的價格是', p[1])
+	count += 1
+
+print('總共買了', count,'樣東西')
+#print('總共花了', ,'元')
